2d1e09f51beef0c69011f88295279ab208cbb4ec
James Dela Cruz <delacruzjamesmartin@gmail.com>
2021-05-27 21:05:49 +0800

    Implement camera

--- App.js
+++ App.js
@@ -1,98 +1,66 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { useEffect, useRef, useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import * as canvas from 'canvas';
-import * as faceapi from 'face-api.js';
-import config from "./config"
-import axios from 'axios';
-
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import { Camera } from "expo-camera";
 
 export default function App() {
-
-  const videoHeight = 480
-  const videoWidth = 640
-  const [initializing, setInitializing] = useState(false)
-  const videoRef = useRef()
-  const canvasRef = useRef()
-  const { Canvas, Image, ImageData } = canvas
-  faceapi.env.monkeyPatch({ Canvas, Image, ImageData })
-  
-  
-  const { PUBLIC_URL } = process.env
-
-  
+  const [hasPermission, setHasPermission] = useState(null);
+  const [type, setType] = useState(Camera.Constants.Type.back);
 
   useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestPermissionsAsync();
+      setHasPermission(status === "granted");
+    })();
+  }, []);
 
-      const loadModels = async () => {
-    
-        setInitializing(true);
-        
-        const net = new faceapi.SsdMobilenetv1()
-        // await net.loadFromUri('./public/models')
-
-        const MODEL_URL = './assets/models'
-      
-        Promise.all([faceapi.nets.tinyFaceDetector.loadFromUri(MODEL_URL)])
-      // Promise.all([
-      //   faceapi.nets.tinyFaceDetector.loadFromUri(MODEL_URL),
-      //   faceapi.nets.faceLandmark68Net.loadFromUri(MODEL_URL),
-      //   faceapi.nets.faceRecognitionNet.loadFromUri(MODEL_URL),
-      //   faceapi.nets.faceExpressionNet.loadFromUri(MODEL_URL)
-      // ]).then(startVideo)
-    }
-    
-    loadModels()
-
-  }, [])
-  
-
-  const startVideo = () => {
-    navigator.getUserMedia({
-      video: {}
-    },
-      (stream) => videoRef.current.srcObject = stream,
-      (err) => console.log(err))
-    
-    
+  if (hasPermission === null) {
+    return <View />;
   }
-
-  const handleVideoOnPlay = () => {
-    setInterval(async() => {
-      
-      if (initializing) {
-        setInitializing(false)
-      }
-
-      const detections = await faceapi.detectAllFaces(videoRef.current, new faceapi.TinyFaceDetectorOptions).withFaceLandmarks().withFaceExpressions()
-
-      console.log(detections)
-    },100)
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
   }
-
-
-  // startVideo()
-
-
-  // startVideo()
   return (
-    <div className="App">
-      <span>{initializing ? 'Initializing' : 'Ready'}</span>
-      <div>
-        <video ref={videoRef} autoPlay muted height={videoHeight} width={videoWidth} onPlay={ handleVideoOnPlay}/>
-        <canvas ref={canvasRef} />
-        <h1>URI{config.PUBLIC_URL+'/models'}</h1>
-      </div>
-     
-   </div>
+    <View style={styles.container}>
+      <Camera style={styles.camera} type={type}>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => {
+              setType(
+                type === Camera.Constants.Type.back
+                  ? Camera.Constants.Type.front
+                  : Camera.Constants.Type.back
+              );
+            }}
+          >
+            <Text style={styles.text}> Flip </Text>
+          </TouchableOpacity>
+        </View>
+      </Camera>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  camera: {
+    flex: 1,
+  },
+  buttonContainer: {
+    flex: 1,
+    backgroundColor: "transparent",
+    flexDirection: "row",
+    margin: 20,
+  },
+  button: {
+    flex: 0.1,
+    alignSelf: "flex-end",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 18,
+    color: "white",
   },
 });
